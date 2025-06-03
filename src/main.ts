805a5177fc71722a47af9ced35fd798e3ec2a9c7
Raaid Tanveer <raaidraiyantanveer@gmail.com>
2025-06-02 17:25:32 -0700

    change webgpu selector

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { assert } from './utils/util';
 
 (async () => {
   if (navigator.gpu === undefined) {
-    const h = document.querySelector('#title') as HTMLElement;
+    const h = document.getElementById('#webgpu-text') as HTMLElement;
     h.innerText = 'WebGPU is not supported in this browser.';
     return;
   }
   const adapter = await navigator.gpu.requestAdapter();
   if (adapter === null) {
-    const h = document.querySelector('#title') as HTMLElement;
+    const h = document.querySelector('#webgpu-text') as HTMLElement;
     h.innerText = 'No adapter is available for WebGPU.';
     return;
   }
